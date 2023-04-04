afecd023d74105a44660f2e1b2e98d671fb40ba5
730443 <730443@605-110-S13>
2023-04-04 08:23:39 -0600

    Add project files.

---
+++ If_statements___Spring_Break_Review.py
@@ -0,0 +1,8 @@
+swol = int(input("How much can you bench press?"))
+if swol < 10:
+    print("Don't worry, you'll get more swole. Keep training")
+elif swol > 10 and swol <= 50:
+    print("You're getting better, keep it up.")
+elif swol > 50:
+    print("YOU ARE MEGA SWOLE")
+
